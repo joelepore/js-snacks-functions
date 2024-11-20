719016602e678d26750eb69c66a92fafade1def8
joelepore <giuseppejoelepore@gmail.com>
2024-11-20 14:25:13 +0100

    feat: complete exercise 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function getInitials(arr) {
+  const initials = [];
+  for (let i = 0; i < arr.length; i++) {
+    initials.push(arr[i][0]);
+  }
+  return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
