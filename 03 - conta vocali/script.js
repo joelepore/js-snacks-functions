dee22001ad2a1c1d4b15344bb45a5fe080ef31c6
joelepore <giuseppejoelepore@gmail.com>
2024-11-20 14:34:40 +0100

    feat: complete exercise 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(word) {
+  word = word.toLowerCase();
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let nVowels = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      nVowels++;
+    }
+  }
+
+  return nVowels;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
